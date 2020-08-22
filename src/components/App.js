fec3c8bd468952c4156509e19f55a19a380a3e94
unknown <vergil1998@hotmail.com>
2020-08-22 12:48:52 +0400

    Said hi to nabeeha xD

--- src/components/App.js
+++ src/components/App.js
@@ -40,6 +40,7 @@ export default function App() {
   const results = data.result.near_earth_objects[day];
   return (
     <div>
+      <h3>HI NABIDOS!</h3>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
         <strong>{results.length}</strong> Meteors
